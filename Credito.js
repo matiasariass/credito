f1820df4e5dbfcb36e281d3f94bb7925c5696bc0
matiasariass <105993400+matiasariass@users.noreply.github.com>
2022-05-28 21:59:39 -0300

    Add files via upload

---
+++ Credito.js
@@ -0,0 +1,51 @@
+function credito() {
+
+
+    let username = prompt("Por favor, ingrese su nombre:")
+    let creditoAceptado = false;
+
+    while(!creditoAceptado) {
+
+        let monto = parseInt(prompt("Ingrese el monto del credito!")) 
+        let montoPermitido = (monto < 30000);
+
+        while(!montoPermitido){
+            monto = parseInt(prompt("Ingreso un monto superior a 30.000, por favor ingrese uno nuevo.")) 
+            montoPermitido = (monto < 30000);
+        }
+
+        let meses = parseInt(prompt("Ingrese la cantidad de cuotas a pagar!"))
+        let plus = 1.50
+        let resultadoTotal = (monto * plus)
+        let resultadoPorMes = ((monto * plus)/meses) 
+
+
+        creditoAceptado = confirm(username + (", Usted en total pagara: " + resultadoTotal))
+
+        if(creditoAceptado)
+            alert("Credito aceptado! Usted pagara: " + resultadoPorMes + " por mes. ¡Gracias por confiar!" ); 
+    }
+    
+
+}
+
+credito()
+
+
+
+// >> Consigna: Con los conocimientos vistos hasta el momento, empezarás a armar la estructura inicial de
+//  tu proyecto integrador. A partir de los ejemplos mostrados la primera clase, deberás:
+// Pensar el alcance de tu proyecto: ¿usarás un cotizador de seguros? ¿un simulador de créditos? ¿un simulador personalizado?
+// Armar la estructura HTML del proyecto.
+// Incorporar al menos un prompt para pedir un dato y luego mostrarlo mediante alert realizando alguna operación.
+// Utilizar funciones para realizar esas operaciones.
+// >>Aspectos a incluir en el entregable:
+// Archivo HTML y Archivo JS, referenciado en el HTML por etiqueta <script src="js/miarchivo.js"></script>, 
+// que incluya la definición de un algoritmo en JavaScript que emplee funciones para resolver el procesamiento principal del simulador
+// >>Ejemplo:
+// Calcular costo total de productos y/o servicios seleccionados por el usuario.
+// Calcular pagos en cuotas sobre un monto determinado.
+// Calcular valor final de un producto seleccionado en función de impuestos y descuentos.
+// Calcular tiempo de espera promedio en relación a la cantidad de turnos registrados.
+// Calcular edad promedio de personas registradas.
+// Calcular nota final de alumnos ingresados.
