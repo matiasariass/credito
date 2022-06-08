3cfa9c0298092abe8d15d2a3b7cd8306d05baf2d
matiasariass <105993400+matiasariass@users.noreply.github.com>
2022-06-08 03:41:33 -0300

    Update simCreditoVol2.js

--- simCreditoVol2.js
+++ simCreditoVol2.js
@@ -2,6 +2,7 @@ function credito() {
     let username = prompt("Por favor, ingrese su nombre:")
     let paisIngresado = prompt('Introduzca su pais')
     let importeInt = calcularInteres(paisIngresado)
+
     let creditoAceptado = false;
   
     while (!creditoAceptado) {
@@ -10,39 +11,39 @@ function credito() {
       let montoPermitido = (monto < 2000000);
   
        while (!montoPermitido) {
-        monto = parseInt(prompt("Ingreso un monto menor a 2.000.000, por favor ingrese uno nuevo."))
+        monto = parseInt(prompt("Ingreso un monto mayor a 2.000.000, por favor ingrese uno nuevo."))
         montoPermitido = (monto < 2000000);
       }
   
       let meses = parseInt(prompt("Ingrese la cantidad de cuotas a pagar!"))
       let impuestoTotal = (0.30 + importeInt)
-      let resultadoTotal = (monto * impuestoTotal)
-      let resultadoPorMes = ((monto * impuestoTotal)/meses) 
+      let resultadoTotal = (monto * impuestoTotal).toFixed(2)
+      let resultadoPorMes = ((monto * impuestoTotal)/meses).toFixed(2)
   
       creditoAceptado = confirm(username + (", Usted en total pagara: " + resultadoTotal))
   
       if (creditoAceptado)
-        alert("Credito aceptado! Usted pagara: " + resultadoPorMes + " por mes. ¡Gracias por confiar!");
+        alert("¡Credito aceptado! Usted pagara: " + resultadoPorMes + " por mes. ¡Gracias por confiar!");
     }
   }
   const paises = [{
-    id: Du5sSkC6MW,
+    id: 1,
     nombre: "Argentina",
     importe: 1.21,
   }, {
-    id: GuN8AIawqU,
+    id: 2,
     nombre: "Colombia",
     importe: 1.31,
   }, {
-    id: SKCDgxtk,
+    id: 3,
     nombre: "Chile",
     importe: 1.91,
   }, {
-    id: Qmur9vWsdF,
+    id: 4,
     nombre: "Brasil",
     importe: 1.41,
   }, {
-    id: fRvFpMys3,
+    id: 5,
     nombre: "Venezuela",
     importe: 1.51,
   }]
@@ -55,28 +56,31 @@ function credito() {
     } else {
       alert("Solo los siguientes paises estan habilitados: Argentina, Brasil, Chile, Uruguay, Venezuela")
     }
+
+
+    // EXTRAER DATOS DE OBJETOS - retorna el id de cada pais
+    let idPaises = paises.map((asd) => {
+      return paises.id;
+      console.table(idPaises)
+    
+});
+// POSIBLE DESCUENTO{
+const paisesConDescuento = paises.map(function (descuento){
+  if (descuento.importe > 1.50) return descuento;
+});
+
+  return{
+      ...descuento,
+      importe: descuento.importe * 0.2
+    };
 }
-// EXTRAER DATOS DE OBJETOS - retorna el id de cada pais
-const idPaises = paises.map(({ id }) => id);
-console.log (idPaises)
+
 
 
 credito()
-
-
-
-
-// POSIBLE DESCUENTO
-//   const paisesConDescuento = select.map(function (descuento){
-//       if (select.importe > 1.50) return select;
-
-//       return{
-//           ...select,
-//           importe: select.importe
-//         };
-//      });
 
   // CONSIGNA
   // estaria faltando un array, un metodo para recorrerlo,
   // puede ser el metodo “map” o mediante algun bucle tipo “forEach, for, for of.. for in..”
   // y un metodo de filtrado que seria el metodo “filter o find”
+  
