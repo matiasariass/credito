a1b92faa1e0f5cc3611281581a7d7bb7ce1b64ea
matiasariass <105993400+matiasariass@users.noreply.github.com>
2022-06-08 01:36:49 -0300

    Update Credito.js

--- Credito.js
+++ Credito.js
@@ -1,32 +1,61 @@
 function credito() {
+  let username = prompt("Por favor, ingrese su nombre:")
+  let paisIngresado = prompt('Introduzca su pais')
+  let importeInt = calcularInteres(paisIngresado)
+  let creditoAceptado = false;
 
+  while (!creditoAceptado) {
 
-    let username = prompt("Por favor, ingrese su nombre:")
-    let creditoAceptado = false;
+    let monto = parseInt(prompt("Ingrese el monto del credito!"))
+    let montoPermitido = (monto < 2000000);
 
-    while(!creditoAceptado) {
-
-        let monto = parseInt(prompt("Ingrese el monto del credito!")) 
-        let montoPermitido = (monto < 300000);
-
-        while(!montoPermitido){
-            monto = parseInt(prompt("Ingreso un monto superior a 300.000, por favor ingrese uno nuevo.")) 
-            montoPermitido = (monto < 300000);
-        }
-
-        let meses = parseInt(prompt("Ingrese la cantidad de cuotas a pagar!"))
-        let plus = 1.50
-        let resultadoTotal = (monto * plus)
-        let resultadoPorMes = ((monto * plus)/meses) 
-
-
-        creditoAceptado = confirm(username + (", Usted en total pagara: " + resultadoTotal))
-
-        if(creditoAceptado)
-            alert("Credito aceptado! Usted pagara: " + resultadoPorMes + " por mes. ¡Gracias por confiar!" ); 
+     while (!montoPermitido) {
+      monto = parseInt(prompt("Ingreso un monto superior a 300.000, por favor ingrese uno nuevo."))
+      montoPermitido = (monto < 2000000);
     }
-    
 
+    let meses = parseInt(prompt("Ingrese la cantidad de cuotas a pagar!"))
+    let impuestoTotal = (0.90 + importeInt)
+    let resultadoTotal = (monto * impuestoTotal / 100)
+    let cuota = monto * (Math.pow(1 + impuestoTotal / 100, meses) * impuestoTotal / 100) / (Math.pow(1 + impuestoTotal / 100, meses) - 1);
+
+
+    creditoAceptado = confirm(username + (", Usted en total pagara: " + resultadoTotal))
+
+    if (creditoAceptado)
+      alert("Credito aceptado! Usted pagara: " + cuota + " por mes. ¡Gracias por confiar!");
+  }
+}
+const paises = [{
+  id: 1,
+  nombre: "Argentina",
+  importe: 1.21,
+}, {
+  id: 2,
+  nombre: "Colombia",
+  importe: 1.31,
+}, {
+  id: 3,
+  nombre: "Chile",
+  importe: 1.91,
+}, {
+  id: 4,
+  nombre: "Brasil",
+  importe: 1.41,
+}, {
+  id: 5,
+  nombre: "Venezuela",
+  importe: 1.51,
+}]
+
+function calcularInteres(paisIngresado) {
+  let resultadoBusqueda = paises.find(pais => pais.nombre === paisIngresado)
+
+  if (resultadoBusqueda) {
+    return resultadoBusqueda.importe
+  } else {
+    alert("Solo los siguientes paises estan habilitados: Argentina, Brasil, Chile, Uruguay, Venezuela")
+  }
 }
 
 credito()
@@ -36,4 +65,6 @@ credito()
 // y un metodo de filtrado que seria el metodo “filter o find”
 
 
+//  let resultadoTotal = (monto * plus)
+//        let resultadoPorMes = ((monto * plus)/meses) 
 
